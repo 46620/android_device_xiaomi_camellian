b605f0944264d9b6eb622004a26535c45e7b8abf
46620 <46620osu@gmail.com>
2022-03-25 23:35:42 -0400

    generated with SebaUbuntu/aospdtgen

---
+++ README.md
@@ -0,0 +1,9 @@
+# Android device tree for Redmi M2103K19C (camellia)
+
+```
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ lineage_camellia.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from camellia device
+$(call inherit-product, device/xiaomi/camellia/device.mk)
+
+PRODUCT_DEVICE := camellia
+PRODUCT_NAME := lineage_camellia
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := M2103K19C
+PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="camellia-user 11 RP1A.200720.011 V12.5.4.0.RKSEUXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/camellia/camellia:11/RP1A.200720.011/V12.5.4.0.RKSEUXM:user/release-keys
