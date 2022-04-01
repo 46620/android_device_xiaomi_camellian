7e0b3c07d4627f0b1e149e278f572214b1b293bd
46620 <46620osu@gmail.com>
2022-04-01 18:42:35 -0400

    [board] verified boot exists

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,6 +102,10 @@ TARGET_USERIMAGES_USE_F2FS := true
 # Security patch level
 VENDOR_SECURITY_PATCH := 2021-08-01
 
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+
 # VINTF
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
